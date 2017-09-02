a7f1343cb1055d12816340a1373785c75719fb0c
Bohdan Khablenko <bkhablenko@gmail.com>
2017-09-02 16:08:40 +0300

    Implement GetWatchBarCount()

--- IvoryTower.lua
+++ IvoryTower.lua
@@ -1,1 +1,11 @@
+local _, IvoryTower = ...
+
+IvoryTower.GetWatchBarCount = function ()
+    local result = 0
+    for _, frame in pairs({ArtifactWatchBar, HonorWatchBar, ReputationWatchBar}) do
+        result = result + (frame:IsShown() and 1 or 0)
+    end
+    return result
+end
+
 LoadAddOn("Blizzard_TalkingHeadUI")
