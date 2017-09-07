55cd767d0d43516893cc1fcf51d1b8f9f25d5bf1
Bohdan Khablenko <bkhablenko@gmail.com>
2017-09-07 23:24:50 +0300

    Use mouse wheel to zoom Minimap

---
+++ IvoryTower_Minimap.lua
@@ -0,0 +1,8 @@
+Minimap:EnableMouseWheel(true)
+Minimap:HookScript("OnMouseWheel", function (self, delta)
+    if (delta > 0) then
+        Minimap_ZoomIn()
+    else
+        Minimap_ZoomOut()
+    end
+end)
