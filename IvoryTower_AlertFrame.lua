86e196f07746171b4b3c23a734c138284b07a187
Bohdan Khablenko <bkhablenko@gmail.com>
2022-01-08 19:42:38 +0200

    Update Interface to 90105

--- IvoryTower_AlertFrame.lua
+++ IvoryTower_AlertFrame.lua
@@ -1,5 +1,5 @@
 -- /run MoneyWonAlertSystem:AddAlert(815)
 
 AlertFrame:ClearAllPoints()
-AlertFrame:SetPoint("BOTTOM", UIParent, "BOTTOM", 0, 150)
+AlertFrame:SetPoint("BOTTOM", UIParent, "BOTTOM", 0, 225)
 AlertFrame.SetPoint = function () end
